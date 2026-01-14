9c6aa561c25b4a3565fccb3e99a8ca0fb41a6ed4
Robicheaux <irobicheauxty@gmail.com>
2026-01-14 12:21:23 -0500

    While Loop ... 95% Stable - Doing Research

--- WithOrWithoutMarinar.py
+++ WithOrWithoutMarinar.py
@@ -1,5 +1,23 @@
 #Welcome Branch
 
+#Libaries Imported Here
+import sys
+import time
+
 print("\nWelcome Branch - Devolper: Ty Robicheaux")
 print("\nWelcome to InfoTechCenter V.1.0")
-print("\nInfoTechCenter is Booting Up...")
+
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    ellipsisMessage = ("InfoTechCenter OS Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys .stdout.write("\r" + ellipsisMessage)
+    time.sleep(.5)
+    if ellipsisMessage == 4:
+        ellipsis = 0
+if x == 20:
+    print("\nOperating System Booted Up - Retina Scanned - Access Granted")
